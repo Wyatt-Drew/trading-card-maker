19d8ebe07e8515238c298b4d81d2036671f05ab4
wyatt-drew <wyatt.j.drew@gmail.com>
2023-12-10 23:25:43 -0600

    Added dataform

--- trading-card-maker/src/App.js
+++ trading-card-maker/src/App.js
@@ -1,46 +1,13 @@
 import React, { useState, useRef } from 'react';
 import html2canvas from 'html2canvas';
+import DataForm from './tradingCardMaker/DataForm';
 
 function App() {
-  const [image, setImage] = useState(null);
-  const [text, setText] = useState("");
-  const cardRef = useRef(null);
 
-  const handleImageChange = (e) => {
-    const file = e.target.files[0];
-    const reader = new FileReader();
-    reader.onloadend = () => {
-      setImage(reader.result);
-    };
-    if (file) {
-      reader.readAsDataURL(file);
-    }
-  };
-
-  const downloadCard = () => {
-    html2canvas(cardRef.current).then((canvas) => {
-      const link = document.createElement("a");
-      link.download = "trading-card.png";
-      link.href = canvas.toDataURL("image/png");
-      link.click();
-    });
-  };
 
   return (
     <div className="App">
-      <input type="file" onChange={handleImageChange} />
-      <input 
-        type="text" 
-        placeholder="Enter Text" 
-        value={text} 
-        onChange={(e) => setText(e.target.value)} 
-      />
-      <div ref={cardRef} style={{ position: 'relative' }}>
-        {image && <img src={image} alt="Uploaded" style={{ width: '300px', height: '400px' }} />}
-        <p style={{ position: 'absolute', bottom: 10, left: 10, color: 'white' }}>{text}</p>
-        {/* {/* Add your template image or style here /} */}
-      </div>
-      <button onClick={downloadCard}>Download Card</button>
+      <DataForm/>
     </div>
   );
 }
