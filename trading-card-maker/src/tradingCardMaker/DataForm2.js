3ceab80025af22d4549d5fbaff3db54c0106116a
wyatt-drew <wyatt.j.drew@gmail.com>
2023-12-13 11:03:45 -0600

    working on dragging

--- trading-card-maker/src/tradingCardMaker/DataForm2.js
+++ trading-card-maker/src/tradingCardMaker/DataForm2.js
@@ -1,72 +1,94 @@
-import React, { useState, useEffect } from 'react';
-import { Stage, Layer, Image } from 'react-konva';
-import backgroundImgPath from '../assets/minecraft_ducky_template.png';
+import React, { useState, useRef, useEffect} from 'react';
+import backgroundImg from '../assets/minecraft_ducky_template.png';
+import html2canvas from 'html2canvas';
 import './DownloadButton.css';
 import './DataForm.css';
 import '../App.css';
 
 const DataForm = () => {
-  // State for draggable text
-  const [dragState, setDragState] = useState({
-    isDragging: false,
-    x: 50,
-    y: 50,
-  });
+    const [image, setImage] = useState(null);
+    const cardRef = useRef(null);
+    const fileInputRef = useRef(null);
+    const [text, setText] = useState("");
 
-  const [image, setImage] = useState(new window.Image());
-  const [dimensions, setDimensions] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-
-  useEffect(() => {
-    const img = new window.Image();
-    img.src = backgroundImgPath;
-    img.onload = () => setImage(img);
-
-    const handleResize = () => {
-      setDimensions({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-  // Function to handle drag start
-  const handleDragStart = () => {
-    setDragState((prevState) => ({
-      ...prevState,
-      isDragging: true,
-    }));
+    const handleImageUpload = (e) => {
+      const file = e.target.files[0];
+      if (file && file.type.startsWith('image/')) {
+          const reader = new FileReader();
+          reader.onload = (e) => {
+              const img = new Image();
+              img.onload = () => {
+                  const canvas = cardRef.current;
+                  canvas.width = img.width;
+                  canvas.height = img.height;
+                  const ctx = canvas.getContext('2d');
+                  ctx.drawImage(img, canvas.width/4, canvas.height/4, canvas.width/2, canvas.height/2);
+              };
+              img.src = e.target.result;
+              setImage(img);
+          };
+          reader.readAsDataURL(file);
+      }
+  };
+  const handleFileInputClick = () => {
+      fileInputRef.current.click();
   };
 
-  // Function to handle drag end
-  const handleDragEnd = (e) => {
-    setDragState({
-      isDragging: false,
-      x: e.target.x(),
-      y: e.target.y(),
-    });
-  };
+    useEffect(() => {
+        // Function to load an image
+        const loadImage = (src) => {
+          return new Promise((resolve, reject) => {
+            const img = new Image();
+            img.src = src;
+            img.onload = resolve;
+            img.onerror = reject;
+          });
+        };
+        // Load the images in sequence
+        const loadImagesInOrder = async () => {
+          const card = cardRef.current;
+          const context = card.getContext('2d');
+          try {
+            await loadImage(backgroundImg);
+            // Draw background image on the canvas here
+            // Set the state to true when both images are loaded
+            const bgImg = new Image();
+            bgImg.src = backgroundImg;
+            bgImg.onload = () => {
+              context.drawImage(bgImg, 0, 0, card.width, card.height);
+            };
+          } catch (error) {
+            console.error("Error loading images", error);
+          }
+        };
+        loadImagesInOrder();
+      }, [backgroundImg, image]);
+    
+    const handleDownloadClick = () => {
+        html2canvas(cardRef.current).then((canvas) => {
+          const link = document.createElement("a");
+          link.download = "trading-card.png";
+          link.href = canvas.toDataURL("image/png");
+          link.click();
+        });
+      };
 
-  return (
-    <Stage width={dimensions.width} height={dimensions.height}>
-      <Layer>
-        <Image
-          image={image}
-          x={dragState.x}
-          y={dragState.y}
-          draggable
-          onDragStart={handleDragStart}
-          onDragEnd={handleDragEnd}
-        />
-      </Layer>
-    </Stage>
-  );
-}
+    return (
+        <div className="container">
+            <div className="card" >
+                <canvas ref={cardRef}/>
+            </div>
+            <div className="form">
+                <input type="file" ref={fileInputRef} style={{ display: 'none' }} onChange={handleImageUpload}/>
+                <button onClick={handleFileInputClick}>Upload Image</button>
+                <input className="input" type="text" placeholder="Enter Text" value={text} onChange={(e) => setText(e.target.value)}/>
+                <button type="button" data-dl onClick={handleDownloadClick}>
+                    <span className="dl-icon"></span>
+                    <span>&#x44;&#x6F;&#x77;&#x6E;&#x6C;&#x6F;&#x61;&#x64;</span>
+                </button>
+            </div>
+        </div>
+    );
+};
 
 export default DataForm;
