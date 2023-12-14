318b95bceaf9fe5f976422140681e11f4781c6a9
wyatt-drew <wyatt.j.drew@gmail.com>
2023-12-14 11:08:53 -0600

    Getting a better scroll mechanism

--- trading-card-maker/src/tradingCardMaker/DataForm.js
+++ trading-card-maker/src/tradingCardMaker/DataForm.js
@@ -1,152 +1,22 @@
-import React, { useState, useRef, useEffect} from 'react';
-import backgroundImg from '../assets/minecraft_ducky_template.png';
-import html2canvas from 'html2canvas';
-import './DownloadButton.css';
-import './DataForm.css';
-import '../App.css';
-import ReactScrollWheelHandler from 'react-scroll-wheel-handler';
+import React, { useState } from 'react';
 
-const DataForm = () => {
-    const [image, setImage] = useState(null);
-    const cardRef = useRef(null);
-    const fileInputRef = useRef(null);
-    const [text, setText] = useState("");
-    const [isDragging, setIsDragging] = useState(false);
-    const [position, setPosition] = useState({ x: 0, y: 0 });
-    const [scale, setScale] = useState(1);
+const WheelComponent = () => {
+  const [scrollDirection, setScrollDirection] = useState('None');
 
-    // Function to increment 'scale'
-    const handleScrollUp = () => {
-      setScale(prevScale => prevScale + 0.1);
-      console.log(scale);
-    };
-  
-    // Function to decrement 'scale'
-    const handleScrollDown = () => {
-      setScale(prevScale => prevScale - 0.1);
-      console.log(scale);
-    };
-    
-    const startDrag = (e) => {
-      setIsDragging(true);
-      updatePosition(e);
+  const handleWheel = (event) => {
+    if (event.deltaY < 0) {
+      setScrollDirection('Up');
+    } else if (event.deltaY > 0) {
+      setScrollDirection('Down');
+    }
   };
 
-  const drag = (e) => {
-      if (isDragging) {
-          updatePosition(e);
-      }
-  };
-
-  const endDrag = () => {
-      setIsDragging(false);
-  };
-
-  const updatePosition = (e) => {
-      if (cardRef.current) {
-          const rect = cardRef.current.getBoundingClientRect();
-          setPosition({
-              x: e.clientX - rect.left,
-              y: e.clientY - rect.top
-          });
-      }
-  };
-
-
-    const handleImageUpload = (e) => {
-      const file = e.target.files[0];
-      if (file && file.type.startsWith('image/')) {
-          const reader = new FileReader();
-          reader.onload = (e) => {
-              const img = new Image();
-              img.onload = () => {
-                  const card = cardRef.current;
-                  card.width = img.width;
-                  card.height = img.height;
-                  const ctx = card.getContext('2d');
-                  ctx.drawImage(img, 0 + position.x, 0 + position.y, (card.width + position.x)*scale, (card.height + position.y)*scale);
-              };
-              img.src = e.target.result;
-              setImage(img);
-          };
-          reader.readAsDataURL(file);
-      }
-  };
-  const handleFileInputClick = () => {
-      fileInputRef.current.click();
-  };
-
-    useEffect(() => {
-        // Function to load an image
-        const loadImage = (src) => {
-          return new Promise((resolve, reject) => {
-            const img = new Image();
-            img.src = src;
-            img.onload = resolve;
-            img.onerror = reject;
-          });
-        };
-        // Load the images in sequence
-        const loadImagesInOrder = async () => {
-          const card = cardRef.current;
-          const context = card.getContext('2d');
-          try {
-            context.clearRect(0, 0, card.width, card.height);
-            if (image) {
-              context.drawImage(image, 0 + position.x, 0 + position.y, (card.width + position.x)*scale, (card.height + position.y)*scale);
-            } 
-            await loadImage(backgroundImg);
-            // Draw background image on the canvas here
-            // Set the state to true when both images are loaded
-            const bgImg = new Image();
-            bgImg.src = backgroundImg;
-            bgImg.onload = () => {
-              context.drawImage(bgImg, 0, 0, card.width, card.height);
-            };
-          } catch (error) {
-            console.error("Error loading images", error);
-          }
-        };
-        loadImagesInOrder();
-      }, [backgroundImg, image, position.y, position.x, scale]);
-    
-    const handleDownloadClick = () => {
-        html2canvas(cardRef.current).then((canvas) => {
-          const link = document.createElement("a");
-          link.download = "trading-card.png";
-          link.href = canvas.toDataURL("image/png");
-          link.click();
-        });
-      };
-
-    return (
-      <ReactScrollWheelHandler
-      upHandler={handleScrollUp}
-      downHandler={handleScrollDown}>
-        <div className="container">
-
-        
-            <div className="card" 
-                onMouseDown={startDrag} 
-                onMouseMove={drag} 
-                onMouseUp={endDrag} 
-                onMouseLeave={endDrag}>
-
-                <canvas ref={cardRef} />
-            </div>
-            <div className="form">
-                <input type="file" ref={fileInputRef} style={{ display: 'none' }} onChange={handleImageUpload}/>
-                <button onClick={handleFileInputClick}>Upload Image</button>
-                <input className="input" type="text" placeholder="Enter Text" value={text} onChange={(e) => setText(e.target.value)}/>
-                <button type="button" data-dl onClick={handleDownloadClick}>
-                    <span className="dl-icon"></span>
-                    <span>&#x44;&#x6F;&#x77;&#x6E;&#x6C;&#x6F;&#x61;&#x64;</span>
-                </button>
-            </div>
-
-        </div>
-        </ReactScrollWheelHandler>
-    );
+  return (
+    <div onWheel={handleWheel} style={{ width: '300px', height: '300px', border: '1px solid black', overflow: 'auto' }}>
+      Scroll inside this box!
+      <p>Scroll Direction: {scrollDirection}</p>
+    </div>
+  );
 };
 
-export default DataForm;
+export default WheelComponent;
